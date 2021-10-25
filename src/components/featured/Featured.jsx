e17cf129667e07cfa7c8899571013184a45cf9d1
EhWanu <cameronjshadgett@gmail.com>
2021-10-25 15:06:44 -0400

    Featured boiler plate

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,3 +1,7 @@
+import {
+	InfoOutlined,
+	PlayArrow,
+} from "@mui/icons-material";
 import "./featured.scss";
 
 const Featured = () => {
@@ -8,6 +12,29 @@ const Featured = () => {
 				src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
 				alt=""
 			/>
+			<div className="info">
+				<img
+					src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1"
+					alt=""
+				/>
+				<span className="desc">
+					Lorem ipsum dolor sit amet consectetur
+					adipisicing elit. Atque ducimus cupiditate
+					asperiores, eos maiores vel quasi molestias
+					delectus ab suscipit quaerat ipsum rem
+					placeat! Ad aliquam deserunt ipsum illo aut.
+				</span>
+				<div className="buttons">
+					<button className="play">
+						<PlayArrow />
+						Play
+					</button>
+					<button className="more">
+						<InfoOutlined />
+						<span>Info</span>
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
