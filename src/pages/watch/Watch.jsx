0a96dcc554821b349383b2102c64a9def6ab6637
EhWanu <cameronjshadgett@gmail.com>
2021-10-27 11:35:41 -0400

    updated imports and redirected folder structure

--- src/pages/watch/Watch.jsx
+++ src/pages/watch/Watch.jsx
@@ -1,7 +1,11 @@
 import "./watch.scss";
 
 const Watch = () => {
-	return <div className="watch"></div>;
+	return (
+		<div className="watch">
+			<div className="back"></div>
+		</div>
+	);
 };
 
 export default Watch;
