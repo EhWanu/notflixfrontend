e251ba7e96642f23fad6227d7fa83b47c1687337
EhWanu <cameronjshadgett@gmail.com>
2021-11-01 14:21:10 -0400

    installed react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,26 @@ import "./app.scss";
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Register";
 import Login from "./pages/login/Login";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+} from "react-router-dom";
 
 function App() {
 	return (
-		<div className="App">
-			<Login />
-		</div>
+		<Switch>
+			<Route path="/about">
+				<About />
+			</Route>
+			<Route path="/users">
+				<Users />
+			</Route>
+			<Route path="/">
+				<Home />
+			</Route>
+		</Switch>
 	);
 }
 
